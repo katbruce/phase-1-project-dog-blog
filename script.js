33d2645f5bd05518a4521b9cbba84e7194e3023b
Michael Marcoux <michaelmarcoux99@gmail.com>
2023-04-02 17:45:56 -0600

    added comment logic

--- script.js
+++ script.js
@@ -40,43 +40,64 @@ function renderPost(post){
     let likeBtn = document.createElement('button');
     let newName = document.createElement('h3');
     let numLikes = document.createElement('p');
+    let postComments = document.createElement('div')
     
-    newPost.className = 'blog-post';
     newPost.id = `post-${post.id}`
     newContent.textContent = post.content;
     newImg.src = post.image;
-    newImg.alt = "dog image";
+    newImg.alt = "Image of a cute dog.";
     newName = post.name;
+    postComments.id = `post-${post.id}-comments`
 
+    //could potentially separate this out
     let nLikes = parseInt(post.likes);
     numLikes.className = 'like-count'
     numLikes.textContent = nLikes;
     likeBtn.textContent = ("♡");//♥
     likeBtn.addEventListener("click", upvote);
     
-    newPost.append(newName, newImg, newContent, likeBtn, numLikes);
+    newPost.append(newName, newImg, newContent, likeBtn, numLikes, postComments);
     allPosts.append(newPost);
 
-    // newPost.append(newName);
-    // newPost.append(newImg);
-    // newPost.append(newContent);
-    // newPost.append(likeBtn);
-    // newPost.append(numLikes);
-    // allPosts.append(newPost);
-    //need to add comments and create comment form
+    //write function to populate comments
+    renderComments(post)
 
+    //write function to render comment form
+    renderCommentForm(post)
+}
+
+function renderComments(postData){
+    let commentBlock = document.querySelector(`#post-${postData.id}-comments`)
+
+    postData.comments.forEach(comment => {
+        let newComment = document.createElement('span')
+        let space = document.createElement('br')
+        newComment.textContent = comment.content
+        commentBlock.append(newComment, space)
+    })
+}
+
+function renderCommentForm(postData){
+    let currentPost = document.querySelector(`#post-${postData.id}`)
+    let commentForm = document.createElement('form')
+    commentForm.className = 'new-comment-form'
+    commentForm.innerHTML = `
+        <input type="text" value="Add comment...">
+        <input type="submit" id="submit">
+    `
+    commentForm.addEventListener('submit', addComment)
+    currentPost.append(commentForm)
 }
 
 //updating number of likes
 function upvote(e){
-    postId = parseInt(e.target.parentNode.id.split('-')[1]);
-    numLikes = parseInt(e.target.nextElementSibling.textContent);
+    let postId = parseInt(e.target.parentNode.id.split('-')[1]);
+    let numLikes = parseInt(e.target.nextElementSibling.textContent);
     numLikes++
     let blogObj = {
         id: postId,
         likes: numLikes
     };
-    console.log(blogObj)
     fetch (`http://localhost:3000/posts/${postId}`, {
         method: 'PATCH',
         headers: {'Content-Type': 'application/json'},
@@ -84,4 +105,22 @@ function upvote(e){
     })
         .then(res => res.json())
         .then(data => e.target.nextElementSibling.textContent = data.likes)
+}
+
+function addComment(e){
+    e.preventDefault()
+    let postId = parseInt(e.target.parentNode.id.split('-')[1]);
+    let newComment = document.createElement('span')
+    newComment.textContent = e.target[0].value
+    let newCommentObj = {
+        id: postId,
+        content: newComment.textContent
+    }
+
+    //not sure what url to post to
+    // fetch (`http://localhost:3000/posts/${postId}`, {
+    //     method: 'POST',
+    //     headers: {'Content-Type': 'application/json'},
+    //     body: JSON.stringify(blogObj)
+    // })
 }
